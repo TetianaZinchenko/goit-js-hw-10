e95810f880b4dfb8725f7996fdd5188086ded2f4
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-01-13 22:27:30 +0200

    changes

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,8 +2,13 @@ const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 const fields = 'fields=name,capital,population,flags,languages';
 
-export function fetchCountries(name) {
-  return fetch(`${BASE_URL}${name}?${fields}`)
-    .then(response => response.json())
-    .catch(error => console.log(error));
+// export function fetchCountries(name) {
+//   return fetch(`${BASE_URL}${name}?${fields}`)
+//     .then(response => response.json())
+//     .catch(error => console.log(error));
+// }
+
+export async function fetchCountries(name) {
+  const response = await fetch(`${BASE_URL}${name}?${fields}`);
+  return await response.json();
 }
